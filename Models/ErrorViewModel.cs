d8d0a642c0f6f4ce35dcb82a6dd13faac1fbbe40
Linda Götenmark <ligo2200@student.miun.se>
2024-02-01 12:10:35 +0100

    new project started moment2_mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace moment2_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/RecipeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace moment2_mvc.Models
+{
+    public class RecipeModel
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public string Instructions { get; set; }
+    }
+}
